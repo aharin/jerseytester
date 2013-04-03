e0d62baf99b28b06b648ff0e1fdfe8d837fd428e
algrimes <agrimes@thoughtworks.com>
2013-04-03 14:05:28 +0100

    Al/Mark - Forcing all get requests to accept text/html

--- jerseytester-webdriver/src/main/java/com/thoughtworks/inproctester/jerseytester/webdriver/JerseyClientHtmlunitDriver.java
+++ jerseytester-webdriver/src/main/java/com/thoughtworks/inproctester/jerseytester/webdriver/JerseyClientHtmlunitDriver.java
@@ -1,12 +1,22 @@
 package com.thoughtworks.inproctester.jerseytester.webdriver;
 
+import com.google.common.net.HttpHeaders;
 import com.sun.jersey.api.client.Client;
 import com.thoughtworks.inproctester.jerseytester.htmlunit.JerseyClientWebConnection;
 import org.openqa.selenium.htmlunit.HtmlUnitDriver;
 
+import javax.ws.rs.core.MediaType;
+
 public class JerseyClientHtmlunitDriver extends HtmlUnitDriver {
     public JerseyClientHtmlunitDriver(Client jerseyClient) {
         getWebClient().setWebConnection(new JerseyClientWebConnection(jerseyClient, getWebClient().getCookieManager()));
+    }
+
+    @Override
+    public void get(String url) {
+        getWebClient().addRequestHeader(HttpHeaders.ACCEPT, MediaType.TEXT_HTML);
+        super.get(url);
+        getWebClient().removeRequestHeader(HttpHeaders.ACCEPT);
     }
 }
 
